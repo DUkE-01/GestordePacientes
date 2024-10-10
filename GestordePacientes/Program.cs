a30249749be401517b0468d35814d4a6f7c38772
Joel gil <joelgil.07@hotmail.com>
2024-10-10 03:14:03 -0400

    changes

--- GestordePacientes/Program.cs
+++ GestordePacientes/Program.cs
@@ -1,6 +1,12 @@
+using GestordePacientes.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options =>
+                            options.UseSqlServer(connectionString));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
